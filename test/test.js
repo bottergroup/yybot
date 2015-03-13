adcfcadb4953f1aa54c9cea8a7eb6fe48fe17e00
huoxin8226 <253522828@qq.com>
2015-03-13 20:31:12 +0800

    Signed-off-by: huoxin8226:tzbtzb123 <253522828@qq.com>

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
 /*
 测试
 */
+var Cmd=require("../lib/tools/commandPaser.js");
+var cmd=new Cmd();
+cmd.setgroup(function(user){if (user=="abc") return true;return false;},{"kill":function(user,command,killed){console.log(user,"kill",killed)}});
+cmd.parse("abc","kill 李大目");
