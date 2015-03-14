a2cea45d7faa18598686f720cef6bc23e69d988a
huoxin8226 <253522828@qq.com>
2015-03-14 14:46:12 +0800

    Signed-off-by: huoxin8226 <253522828@qq.com>

--- test/test.js
+++ test/test.js
@@ -3,5 +3,5 @@
 */
 var Cmd=require("../lib/tools/commandPaser.js");
 var cmd=new Cmd();
-cmd.setgroup(function(user,room){if ((user=="abc")&&(room==undefined)) return true;return false;},{"kill":function(user,room,command,killed){console.log(user,"kill",killed)}});
-cmd.parse("abc",undefined,"kill    李大目");
+cmd.setgroup(function(user,room){if ((user=="abc")&&(room==undefined)) return true;return false;},{"kill":function(user,room,command,killed){return user+" kill "+killed;}});
+if ((cmd.parse("abc",undefined,"kill             李大目"))=="abc kill 李大目") {console.log("commandPaser ok")} else console.log("commandPaser wrong!");
