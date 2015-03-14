066fe80c0c393d0ac976b6561b221c58a51cebd7
huoxin8226 <253522828@qq.com>
2015-03-14 10:20:39 +0800

    房间参数
    Signed-off-by: huoxin8226 <253522828@qq.com>

--- test/test.js
+++ test/test.js
@@ -3,5 +3,5 @@
 */
 var Cmd=require("../lib/tools/commandPaser.js");
 var cmd=new Cmd();
-cmd.setgroup(function(user){if (user=="abc") return true;return false;},{"kill":function(user,command,killed){console.log(user,"kill",killed)}});
-cmd.parse("abc","kill    李大目");
+cmd.setgroup(function(user,room){if ((user=="abc")&&(room==undefined)) return true;return false;},{"kill":function(user,room,command,killed){console.log(user,"kill",killed)}});
+cmd.parse("abc",undefined,"kill    李大目");
