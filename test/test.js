51966622d562f593a3775b29da93bb7575bba821
huoxin8226 <253522828@qq.com>
2015-03-14 20:28:45 +0800

    效验信息作为对象，增强通用性
    Signed-off-by: huoxin8226 <253522828@qq.com>

--- test/test.js
+++ test/test.js
@@ -3,5 +3,5 @@
 */
 var Cmd=require("../lib/tools/commandPaser.js");
 var cmd=new Cmd();
-cmd.setgroup(function(user,room){if ((user=="abc")&&(room==undefined)) return true;return false;},{"kill":function(user,room,command,killed){return user+" kill "+killed;}});
-if ((cmd.parse("abc",undefined,"kill             李大目"))=="abc kill 李大目") {console.log("commandPaser ok")} else console.log("commandPaser wrong!");
+cmd.setgroup(function(verinfo){if ((verinfo.user=="狗熊")&&(verinfo.room=="游戏1")) return true;return false;},{"杀死":function(verinfo,command,killed){return "to"+verinfo.room+":"+verinfo.user+" kill "+killed;}});
+if (cmd.parse({user:"狗熊",room:"游戏1"},"杀死             李大目")=="to游戏1:狗熊 kill 李大目") {console.log("commandPaser ok")} else console.log("commandPaser wrong!");
