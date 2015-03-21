7b2039f974d7bddf329701f44471e03664548609
huoxin8226 <253522828@qq.com>
2015-03-21 17:03:59 +0800

    Signed-off-by: huoxin8226 <253522828@qq.com>

--- test/test.js
+++ test/test.js
@@ -1,7 +1,36 @@
 /*
 测试
 */
-var Cmd=require("../lib/tools/commandPaser.js");
-var cmd=new Cmd();
-cmd.setgroup(function(verinfo){if ((verinfo.user=="狗熊")&&(verinfo.room=="游戏1")) return true;return false;},{"杀死":function(verinfo,command,killed){return "to"+verinfo.room+":"+verinfo.user+" kill "+killed;}});
-if (cmd.parse({user:"狗熊",room:"游戏1"},"杀死             李大目")=="to游戏1:狗熊 kill 李大目") {console.log("commandPaser ok")} else console.log("commandPaser wrong!");
+"use strict";
+var assert=require("assert");
+
+describe("commandPaser",function(){
+	it("commandPaser test",function(){
+		var Cmd=require("../lib/tools/commandPaser.js");
+		var cmd=new Cmd();
+		cmd.setgroup(function(verinfo){
+			if ((verinfo.user=="狗熊")&&(verinfo.room=="游戏1")) return true;return false;
+		},
+		{
+			"杀死":
+			function(verinfo,command,killed){
+				return "to "+verinfo.room+":"+verinfo.user+" kill "+killed;
+			}
+		});
+		assert.equal(cmd.parse({user:"狗熊",room:"游戏1"},"杀死             李大目"),"to 游戏1:狗熊 kill 李大目");
+	});
+});
+describe("io",function(){
+	it("io test",function(){
+		var Io=require("../lib/tools/io.js");
+		var io=new Io(["user"]);
+		var outerr=function(err){console.log(err);done();};
+		io.updataTablesAsync(["user"]).then(function(){
+			io.setAsync("user","me","b").then(function(){
+				io.getAsync("user","me").then(function(res){
+					console.log(res[0]);
+				}).catch(outerr);
+			});
+		});
+	});
+});
