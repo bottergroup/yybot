2d506ed11d4cc2ac452615e2555393af05e7f456
huoxin8226 <253522828@qq.com>
2015-03-21 19:14:04 +0800

    Signed-off-by: huoxin8226 <253522828@qq.com>

--- test/test.js
+++ test/test.js
@@ -23,19 +23,21 @@ describe("commandPaser",function(){
 		assert.equal(cmd.parse({user:"狗熊",room:"游戏1"},"杀死             李大目"),"to 游戏1:狗熊 kill 李大目");
 	});
 });
+var r;
 describe("io",function(){
 	var Io;
 	it("io load",function(){
 		Io=require("../lib/tools/io.js");
 	});
-	it("io run",function(){
+	it("io run",function(done){
 		var io=new Io(["user"]);
-		var outerr=function(err){console.log(err);done();};
 		io.updataTablesAsync(["user"]).then(function(){
 			io.setAsync("user","me","b").then(function(){
 				io.getAsync("user","me").then(function(res){
-					console.log(res[0]);
-				}).catch(outerr);
+					assert.equal(res[0].key,"me");
+					assert.equal(res[0].value,"b");
+					done();
+				}).catch(function(err){if (err!=undefined) {console.log(err);}done();});
 			});
 		});
 	});
